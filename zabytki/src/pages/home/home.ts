0ca06fd9c9acda968290b81a02369f6c64b69895
blanka <blasznyt@student.pg.gda.pl>
2018-10-17 21:56:03 +0200

    Marker na środku z popupem

--- zabytki/src/pages/home/home.ts
+++ zabytki/src/pages/home/home.ts
@@ -27,7 +27,6 @@ export class HomePage {
     this.displayGoogleMap();
   }
 
-
   displayGoogleMap() {
     this.geolocation.getCurrentPosition().then((position) => {
       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
@@ -53,9 +52,34 @@ export class HomePage {
         ]
       }
       this.map = new google.maps.Map(this.mapContainer.nativeElement, mapOptions);
+      this.addMarker();
     }, (err) => {
       console.log(err);
     });
+  }
+
+  addMarker() {
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+  }
+
+  addInfoWindow(marker, content){
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
   }
 
 
